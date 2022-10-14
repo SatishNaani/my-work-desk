cda6c7ca78fdc6cabd6fdbb332ed4d8a43461e1e
satish.medhuri <satish.medhuri@vistex.com>
2022-10-14 12:42:18 +0530

    Second change

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,31 +3,9 @@ import Box from '@mui/material/Box';
 import {Avatar, Paper, Stack} from "@mui/material";
 import UserDetails from "./userDetails";
 
-function stringToColor(string) {
-    let hash = 0;
-    let i;
-
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-        hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = '#';
-
-    for (i = 0; i < 3; i += 1) {
-        const value = (hash >> (i * 8)) & 0xff;
-        color += `00${value.toString(16)}`.slice(-2);
-    }
-    /* eslint-enable no-bitwise */
-
-    return color;
-}
 
 function stringAvatar(name) {
     return {
-        sx: {
-            bgcolor: stringToColor(name),
-        },
         children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
     };
 }
@@ -36,27 +14,19 @@ function NavBar() {
     return (
         <div style={{position: "fixed", zIndex: 50, height: '100%'}}>
             <Box sx={{
-                width: 198,
+                width: 218,
                 height: '100%',
-                backgroundColor: 'white',
-                '&:hover': {
-                    backgroundColor: 'white',
-                    opacity: [0.9, 0.8, 0.7],
-                },
+                backgroundColor: 'white'
             }}>
                 <Paper elevation={6} sx={{
-                    width: 198,
+                    width: 218,
                     height: '100%',
-                    backgroundColor: 'white',
-                    '&:hover': {
-                        backgroundColor: 'white',
-                        opacity: [0.9, 0.8, 0.7],
-                    },
+                    backgroundColor: 'white'
                 }}>
-                    <div style={{width: '200px', top: '50px', position: 'relative'}}>
+                    <div style={{width: '220px', top: '50px', position: 'relative'}}>
                         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                             <Stack direction="row" spacing={2}>
-                                <Avatar {...stringAvatar('Satish Medhuri')} sx={{width: 85, height: 85}}/>
+                                <Avatar style ={{background:'#1976d2'}} {...stringAvatar('Satish Medhuri')} sx={{width: 85, height: 85}}/>
                             </Stack>
                         </div>
                     </div>
--- src/components/userDetails.js
+++ src/components/userDetails.js
@@ -1,30 +1,81 @@
 import React, {useState} from 'react'
 import Box from '@mui/material/Box';
-import {Paper} from "@mui/material";
+import {Divider, ListItemIcon, ListItemText, MenuItem, MenuList, Paper} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import {ContentCut} from "@mui/icons-material";
 
 function UserDetails() {
     return (
         <div className="userDetails">
-            <form>
-                <div>
-                    <label>
-                        User Name
-                    </label>
+            <form style={{width: '210px'}}>
+                <div className='userName'>
+                    <div>
+                        <label>
+                            <Typography variant="inherit" noWrap>
+                                User Name
+                            </Typography>
+                        </label>
+
+                    </div>
+                    <div>
+                        <label>
+                            <Typography variant="inherit" noWrap>
+                                User.Email@vistex.com
+                            </Typography>
+                        </label>
+
+                    </div>
+                    <div>
+                        <label>
+                            <Typography variant="inherit" noWrap>
+                                User Phone
+                            </Typography>
+                        </label>
+
+                    </div>
+                </div>
+                <div style={{position: "relative", top: "70px"}}>
+                    <div style={{textAlign: "center"}}><label> List of Projects</label></div>
+
+                    <MenuList>
+                        <MenuItem style={{
+                            background: 'rgba(0, 0, 0, 0.04)',
+                            borderBottom: '1px solid rgba(0, 0, 0, 0.09)'
+                        }}>
+                            <Typography variant="inherit" noWrap>
+                                Project 1
+                            </Typography>
+                        </MenuItem>
+                        <MenuItem style={{
+                            background: 'rgba(0, 0, 0, 0.04)',
+                            borderBottom: '1px solid rgba(0, 0, 0, 0.09)'
+                        }}>
+                            <Typography variant="inherit" noWrap>
+                                Project 2
+                            </Typography>
+                        </MenuItem>
+                        <MenuItem style={{
+                            background: 'rgba(0, 0, 0, 0.04)',
+                            borderBottom: '1px solid rgba(0, 0, 0, 0.09)'
+                        }}>
+                            <Typography variant="inherit" noWrap>
+                                Project 3
+                            </Typography>
+                        </MenuItem>
+                        <MenuItem style={{
+                            background: 'rgba(0, 0, 0, 0.04)',
+                            borderBottom: '1px solid rgba(0, 0, 0, 0.09)'
+                        }}>
+                            <Typography variant="inherit" noWrap>
+                                Project 4
+                            </Typography>
+                        </MenuItem>
+                    </MenuList>
 
                 </div>
-                <div>
 
-                    <label>
-                        User Email
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        User Phone
-                    </label>
-
-                </div>
             </form>
+
 
         </div>
     )
